0f3e37ec8e01917fbbe4dc4cd7ea75c87b0fe762
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-07-28 18:08:34 +0200

    struggling on the imports

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/*
 import component from './component';
 
 //image
@@ -10,5 +11,19 @@ import './styles/woof.css';
 const img = document.createElement('img');
 img.src = sweat;
 document.body.appendChild(img);
-
 document.body.appendChild(component());
+*/
+
+let pageArgument;
+
+const setRoute = () => {
+  let path = window.location.hash.substring(1).split("/");
+  pageArgument = path[1] || "";
+
+  var pageContent = document.getElementById("pageContent");
+  routes[path[0]](pageArgument);
+  return true;
+};
+
+window.addEventListener("hashchange", () => setRoute());
+window.addEventListener("DOMContentLoaded", () => setRoute());
